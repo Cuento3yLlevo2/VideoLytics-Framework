6e50daf51766f5467dafd6d933b13c02a0fe3c55
Cuento3yLlevo2 <mhermoso1993@gmail.com>
2021-07-03 13:29:34 +0200

    Now Video Playback syncs with the Activity lifecycle. onPause() the Playback stops and the frame where it stopped is recorded. onResume() the Playback continues from when it was left.  Implemented getPlayerError() to listen for any error that caused playback to fail.

--- app/src/main/java/com/mahi/videolytics/MainActivity.kt
+++ app/src/main/java/com/mahi/videolytics/MainActivity.kt
@@ -2,68 +2,38 @@ package com.mahi.videolytics
 
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.ui.StyledPlayerControlView
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.HttpDataSource.HttpDataSourceException
+import com.google.android.exoplayer2.upstream.HttpDataSource.InvalidResponseCodeException
+import com.google.android.exoplayer2.util.Log
 import com.google.android.exoplayer2.util.Util
+import java.io.IOException
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, StyledPlayerControlView.VisibilityListener {
+class MainActivity : AppCompatActivity() {
 
     // Variable Represents ExoPlayer
     private var exoplayer: SimpleExoPlayer? = null
-    private var playWhenReady = true
-    private var playbackPosition : Long = 0
-    private var currentWindow = 0
+
     private lateinit var playerView: StyledPlayerView
+    private var playWhenReady = true
+    private var startWindow = 0
+    private var startPosition: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         playerView = findViewById(R.id.spvStyledPlayerView)
-
-        initExoplayer()
-
-        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(this)
-
-        // I'm using the following video resource as suggested
-        // http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8
-        val videoUri : Uri = Uri.parse("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8")
-
-        val hlsMediaSource: HlsMediaSource = HlsMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(MediaItem.fromUri(videoUri))
-
-        /*
-        val field : Field = R.raw::class.java.getField("catlicking")
-        val uri = Uri.parse(
-            "android.resource://" + packageName + "/" + resources.getIdentifier(
-                field.name,
-                "raw",
-                packageName
-            )
-        )
-        val finalUri = Uri.parse("https://i.imgur.com/7bMqysJ.mp4")
-
-        // Build the media item needed to populate Exoplayer View.
-        val mediaItem: MediaItem = MediaItem.fromUri(uri)
-        */
-
-
-        // Set the MediaSource to be played.
-        exoplayer?.setMediaSource(hlsMediaSource)
-        // Prepare the player.
-        exoplayer?.prepare()
-        // Start the playback.
-        exoplayer?.playWhenReady
-
-        // exoplayer?.seekTo(currentWindow, playbackPosition)
-
     }
 
     private fun initExoplayer() {
@@ -71,14 +41,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, StyledPlayerCont
         exoplayer = SimpleExoPlayer.Builder(this).build()
         // Here we bind the player to the view.
         exoplayer.let { playerView.player = it }
+
+        // I'm using the following video resource as suggested
+        // http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(this)
+        val videoUri : Uri = Uri.parse("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8")
+        val hlsMediaSource: HlsMediaSource = HlsMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(videoUri))
+
+        // Set the MediaSource to be played.
+        exoplayer?.setMediaSource(hlsMediaSource, true)
+        // Prepare the player.
+        exoplayer?.prepare()
+        // Start the playback.
+        exoplayer?.playWhenReady = playWhenReady
+
+        exoplayer?.seekTo(startWindow, startPosition)
+
+        // When a failure occurs, this method will be called
+        exoplayer?.playerError?.let { onPlayerError(it) }
     }
 
     override fun onStart() {
         super.onStart()
-        /*
-        if(Util.SDK_INT >= 24){
-            initExoplayer()
-        }*/
+        initExoplayer()
     }
 
     override fun onResume() {
@@ -89,55 +75,51 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, StyledPlayerCont
         }
     }
 
-
     private fun hideSystemUI() {
-        playerView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        )
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, findViewById(R.id.layoutRoot)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 
     override fun onPause() {
-        if(Util.SDK_INT < 24){
-            releasePlayer()
-        }
+        releasePlayer()
         super.onPause()
-
     }
 
     private fun releasePlayer() {
         if(exoplayer != null){
             playWhenReady = exoplayer!!.playWhenReady
-            playbackPosition = exoplayer!!.currentPosition
-            currentWindow = exoplayer!!.currentWindowIndex
+            startPosition = exoplayer!!.currentPosition
+            startWindow = exoplayer!!.currentWindowIndex
             exoplayer!!.release()
             exoplayer = null
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        exoplayer?.release()
+    private fun onPlayerError(error: ExoPlaybackException) {
+        if (error.type == ExoPlaybackException.TYPE_SOURCE) {
+            val cause: IOException = error.sourceException
+            if (cause is HttpDataSourceException) {
+                // An HTTP error occurred.
+                // querying the cause.
+                if (cause is InvalidResponseCodeException) {
+                    // Cast to InvalidResponseCodeException and retrieve the response code,
+                    // message and headers.
+                    cause.message?.let { Log.d("onPlayerError", it) }
+                } else {
+                    // Try calling httpError.getCause() to retrieve the underlying cause,
+                    // although note that it may be null.
+                    cause.message?.let { Log.d("onPlayerError", it) }
+                }
+            }
+        }
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onVisibilityChange(visibility: Int) {
-        TODO("Not yet implemented")
-    }
-
-    companion object {
-        // Saved instance state keys.
-        const private val KEY_TRACK_SELECTOR_PARAMETERS = "track_selector_parameters"
-        const private val KEY_WINDOW = "window"
-        const private val KEY_POSITION = "position"
-        const private val KEY_AUTO_PLAY = "auto_play"
+    override fun onStop() {
+        releasePlayer()
+        super.onStop()
     }
 
 }
